6b128f571c47ce21f83291d1f84af6ece3ca2e97
GergelyTimeea <timeea.gergely@student.upt.ro>
2025-02-05 17:34:41 +0200

    I added what was left to do

--- cypress/e2e/ownWebsite-singleRoom.cy.js
+++ cypress/e2e/ownWebsite-singleRoom.cy.js
@@ -4,7 +4,7 @@ describe("Booking Flow", () => {
   it("should complete booking", () => {
     // Visit the specified URL
     cy.visit(
-      "https://visitor-qa.visitor.de/?language=en-US&checkinDate=01-01-2026&checkoutDate=02-01-2026"
+       "https://visitor-qa.visitor.de/?language=en-US&checkinDate=01-01-2026&checkoutDate=02-01-2026"
     );
 
     // Select the number of adults and children
@@ -14,7 +14,7 @@ describe("Booking Flow", () => {
     cy.get('input[name="childrenAges[1].age"]').should("exist").clear().type("7");
 
     // Wait for "0 Room(s)" to appear
-    cy.contains("p", "0 Room(s)").should("be.visible");
+    cy.contains("p", "0 Room(s)").should("be.visible"); 
 
     // Add a room
     cy.contains("h4", "Triple Room")
@@ -29,12 +29,13 @@ describe("Booking Flow", () => {
       });
 
     // Compare costs on different pages
-    //De asta nu e nevoie
+
     cy.get('[data-testid="final-cost"]')
       .should("be.visible")
       .invoke("text")
       .then((initialFinalCost) => {
         const firstPageCost = initialFinalCost.trim();
+
         cy.get('[data-testid="next-step-btn"]')
           .should("exist")
           .scrollIntoView()
@@ -46,23 +47,30 @@ describe("Booking Flow", () => {
           .then((nextPageFinalCost) => {
             const secondPageCost = nextPageFinalCost.trim();
             expect(firstPageCost).to.equal(secondPageCost);
-            //.
+          
 
             cy.get('input[name="firstName"]').should("exist").clear().type("Popescu");
             cy.get('input[name="lastName"]').should("exist").clear().type("Maria");
             cy.get('input[name="phoneNumber"]').should("exist").clear().type("0712345678");
             cy.get('input[name="email"]').should("exist").clear().type("maria@gmail.com");
 
+            cy.get('input[id="roomBookings[0].numberOfAdults"]').should("have.value", "1");
+
+            cy.get('input[id="roomBookings[0].numberOfChildren"]').should("have.value", "2");
+
+           // cy.wait(10000); 
+
             cy.get(`[data-testid="next-step-btn"]`)
               .should("exist")
               .scrollIntoView()
               .click({ force: true });
+
           });
       });
   });
 
   it("should log in and verify booking details", () => {
-   
+
     cy.clearCookies();
     cy.clearLocalStorage();
     cy.visit("https://app.visitor.de/login");
@@ -87,6 +95,22 @@ describe("Booking Flow", () => {
 
     cy.get('[data-testid="edit-btn"]').click();
 
+    cy.get('input[name="customer.fullName"]').should("have.value", "Popescu Maria");
+
+    cy.get('input[name="customer.email"]').should("have.value", "maria@gmail.com");
+
+    cy.get('input[name="customer.phoneNumber"]').should("have.value", "0712345678");
+
+    cy.get('#mui-component-select-bookingSource').should('contain.text', 'Website');
+
+    cy.get('input[placeholder="DD/MM/YYYY"]').eq(0).should('have.value', '01/01/2026');
+
+    cy.get('input[placeholder="DD/MM/YYYY"]').eq(1).should('have.value', '02/01/2026');
+
+    cy.get('input[id="roomBookings[0].numberOfAdults"]').should("have.value", "1");
+
     cy.get('input[id="roomBookings[0].numberOfChildren"]').should("have.value", "2");
+
+    cy.get('input[id="roomBookings[0].pricePerNight"]').should("have.value", "139");
   });
-});
+})
